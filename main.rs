20f7c351ccc76c31920b62698cd0156a8c4505c9
Piripant <piripant@gmail.com>
2019-04-04 21:54:27 +0200

    Render texture now has minimum size possible

--- main.rs
+++ main.rs
@@ -14,7 +14,6 @@ impl World {
 
     fn add_arm(&mut self, length: f64, velocity: f64) {
         self.arms.push((length, velocity));
-        println!("{:?}", self.max_length());
     }
 
     fn get_positions(&self, time: f64) -> Vec<(f64, f64)> {
@@ -33,14 +32,6 @@ impl World {
         }
 
         positions
-    }
-
-    // TODO: Actually compute the max height/width
-    fn max_length(&self) -> (f64, f64) {
-        (
-            self.arms.iter().fold(0.0, |sum, (length, _)| sum + length),
-            self.arms.iter().fold(0.0, |sum, (length, _)| sum + length),
-        )
     }
 }
 
@@ -87,11 +78,12 @@ fn main() {
     let mut arms_input: Vec<pingui::MultiBox> = Vec::new();
 
     let mut world = World::new();
-    let mut draw_image = ::image::RgbaImage::new(1, 1);
+    let mut draw_image =
+        ::image::RgbaImage::new(window.size().width as u32, window.size().height as u32);
     let mut draw_texture = Texture::from_image(
         &mut window.factory,
         &draw_image,
-        &TextureSettings::new().filter(Filter::Nearest),
+        &TextureSettings::new().filter(Filter::Nearest).mag(Filter::Nearest),
     )
     .unwrap();
 
@@ -100,29 +92,21 @@ fn main() {
     let mut time = 0.0;
     let mut speed = 1;
     let mut running = true;
-    let mut update_image = false;
     while let Some(e) = window.next() {
         gui.event(&e);
 
-        if update_image {
-            let dimensions = world.max_length();
-            draw_image = ::image::RgbaImage::new(
-                dimensions.0.ceil() as u32 * 2 + 10,
-                dimensions.1.ceil() as u32 * 2 + 10,
-            );
+        if let Some(dimensions) = e.resize_args() {
+            draw_image = ::image::RgbaImage::new(dimensions[0] as u32, dimensions[1] as u32);
             draw_texture = Texture::from_image(
                 &mut window.factory,
                 &draw_image,
-                &TextureSettings::new()
-                    .filter(Filter::Linear)
-                    .mag(Filter::Linear),
+                &TextureSettings::new().filter(Filter::Nearest),
             )
             .unwrap();
-            update_image = false;
         }
 
-        let offset_x = window.size().width / 2.0;
-        let offset_y = window.size().height / 2.0;
+        let offset_x = f64::from(draw_image.width()) / 2.0;
+        let offset_y = f64::from(draw_image.height()) / 2.0;
         if let Some(args) = e.update_args() {
             if running {
                 let iterations = speed as usize;
@@ -169,14 +153,10 @@ fn main() {
             for i in 0..new_trace.len() - 1 {
                 let a = new_trace[i];
                 let b = new_trace[i + 1];
-                let a = (
-                    a.0 + draw_image.width() as f64 / 2.0 + 0.5,
-                    a.1 + draw_image.height() as f64 / 2.0 + 0.5,
-                );
-                let b = (
-                    b.0 + draw_image.width() as f64 / 2.0 + 0.5,
-                    b.1 + draw_image.height() as f64 / 2.0 + 0.5,
-                );
+                let a = (a.0 + offset_x + 0.5, a.1 + offset_y + 0.5);
+                let b = (b.0 + offset_x + 0.5, b.1 + offset_y + 0.5);
+                //let a = (0.0, 0.0);
+                //let b = (50.0, 25.0);
                 draw_line(&mut draw_image, &a, &b, [64, 64, 64, 255]);
             }
 
@@ -193,14 +173,7 @@ fn main() {
                 clear([1.0; 4], g);
 
                 // Draw the trace
-                image(
-                    &draw_texture,
-                    c.transform.trans(
-                        offset_x - draw_image.width() as f64 / 2.0,
-                        offset_y - draw_image.height() as f64 / 2.0,
-                    ),
-                    g,
-                );
+                image(&draw_texture, c.transform, g);
 
                 if !positions.is_empty() {
                     // Draw the arms
@@ -220,11 +193,9 @@ fn main() {
 
                 // GUI
                 if clear_button.is_pressed() {
-                    time = 0.0;
                     for pixel in draw_image.pixels_mut() {
                         *pixel = ::image::Rgba([0, 0, 0, 0]);
                     }
-                    new_trace.clear();
                 }
 
                 for (i, arm_input) in &mut arms_input.iter().enumerate() {
@@ -233,10 +204,7 @@ fn main() {
                     arm_input.input(0, &mut length);
                     arm_input.input(1, &mut velocity);
 
-                    if (length, velocity) != world.arms[i] {
-                        world.arms[i] = (length, velocity);
-                        update_image = true;
-                    }
+                    world.arms[i] = (length, velocity);
                 }
 
                 if add_button.is_pressed() {
@@ -250,12 +218,10 @@ fn main() {
 
                     arms_input.push(inputbox);
                     world.add_arm(25.0, 90.0);
-                    update_image = true;
                 }
                 if remove_button.is_pressed() {
                     arms_input.pop();
                     world.arms.pop();
-                    update_image = true;
                 }
 
                 simulation_label.title = format!("Sim speed: {}x", speed);
@@ -272,40 +238,77 @@ fn main() {
     }
 }
 
+fn ipart(x: f64) -> f64 {
+    x.floor()
+}
+
+fn round(x: f64) -> f64 {
+    ipart(x + 0.5)
+}
+
+fn fpart(x: f64) -> f64 {
+    x - ipart(x)
+}
+
+fn rfpart(x: f64) -> f64 {
+    1.0 - fpart(x)
+}
+
+fn draw_point(image: &mut ::image::RgbaImage, x: f64, y: f64, c: f64) {
+    let alpha = (c * 255.0).min(255.0).max(0.0) as u8;
+    let color = [255, 0, 0, alpha];
+    image.put_pixel(x as u32, y as u32, ::image::Rgba(color));
+}
+
 fn draw_line(image: &mut ::image::RgbaImage, from: &(f64, f64), to: &(f64, f64), color: [u8; 4]) {
-    use imageproc::drawing;
+    let (mut x1, mut y1) = from;
+    let (mut x2, mut y2) = to;
 
-    let line_thickness = 1.5;
+    let mut dx = x2 - x1;
+    let mut dy = y2 - y1;
+    let ax = dx.abs();
+    let ay = dy.abs();
 
-    let delta = (to.0 - from.0, to.1 - from.1);
-    let length = (delta.0.powi(2) + delta.1.powi(2)).sqrt();
+    let mut plot: Box<FnMut(f64, f64, f64)>;
+    if ax < ay {
+        std::mem::swap(&mut x1, &mut y1);
+        std::mem::swap(&mut x2, &mut y2);
+        std::mem::swap(&mut dx, &mut dy);
+        plot = Box::new(|x, y, c| {
+            draw_point(image, y, x, c);
+        });
+    } else {
+        plot = Box::new(|x, y, c| {
+            draw_point(image, x, y, c);
+        });
+    }
+    if x2 < x1 {
+        std::mem::swap(&mut x1, &mut x2);
+        std::mem::swap(&mut y1, &mut y2);
+    }
+    let gradient = dy / dx;
 
-    if length != 0.0 {
-        let normal = (
-            delta.1 / length * line_thickness,
-            -delta.0 / length * line_thickness,
-        );
+    let xend = round(x1);
+    let yend = y1 + gradient * (xend - x1);
+    let xgap = rfpart(x1 + 0.5);
+    let xpxl1 = xend;
+    let ypxl1 = ipart(yend);
+    plot(xpxl1, ypxl1, rfpart(yend) * xgap);
+    plot(xpxl1, ypxl1 + 1.0, fpart(yend) * xgap);
+    let mut intery = yend + gradient;
 
-        let points = vec![
-            drawing::Point::new(
-                (from.0 - normal.0 / 2.0).floor() as i32,
-                (from.1 - normal.1 / 2.0).floor() as i32,
-            ),
-            drawing::Point::new(
-                (to.0 - normal.0 / 2.0).floor() as i32,
-                (to.1 - normal.1 / 2.0).floor() as i32,
-            ),
-            drawing::Point::new(
-                (to.0 + normal.0 / 2.0).floor() as i32,
-                (to.1 + normal.1 / 2.0).floor() as i32,
-            ),
-            drawing::Point::new(
-                (from.0 + normal.0 / 2.0).floor() as i32,
-                (from.1 + normal.1 / 2.0).floor() as i32,
-            ),
-        ];
+    let xend = round(x2);
+    let yend = y2 + gradient * (xend - x2);
+    let xgap = fpart(x2 + 0.5);
+    let xpxl2 = xend;
+    let ypxl2 = ipart(yend);
+    plot(xpxl2, ypxl2, rfpart(yend) * xgap);
+    plot(xpxl2, ypxl2 + 1.0, fpart(yend) * xgap);
 
-        drawing::draw_convex_polygon_mut(image, &points, ::image::Rgba(color))
+    for x in (xpxl1 as u32 + 1)..(xpxl2 as u32 - 1) {
+        plot(x as f64, ipart(intery), rfpart(intery));
+        plot(x as f64, ipart(intery), rfpart(intery));
+        intery += gradient;
     }
 }
 
@@ -340,5 +343,7 @@ fn save(image: &::image::RgbaImage) {
     let cropped_image =
         ::image::imageops::crop(&mut image.clone(), start_x, start_y, width + 1, height + 1)
             .to_image();
+    
+    
     cropped_image.save("save.png").unwrap();
 }
